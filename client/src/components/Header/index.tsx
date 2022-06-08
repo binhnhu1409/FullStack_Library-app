f482616160b939310a172c210a3f2edbe79cd453
binhnhu1409 <binhnhu1409@gmail.com>
2022-06-09 00:47:04 +0300

    Header components done

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+// import { useSelector } from 'react-redux'
+
+import { BsFillBookmarkHeartFill } from 'react-icons/bs'
+
+import './header.scss'
+// import { ThemeContext } from '../../context/theme'
+// import { SidebarContext } from '../../context/sidebar'
+// import { AppState } from '../../types'
+
+const Header = () => {
+
+  return (
+    <header id='homepage'>
+      <nav className="navbar">
+
+        {/* logo on the left  */}
+        <button className='navbar__logo'>
+          <a href='#homepage'>Tiny Library</a>
+        </button>
+
+        {/* menu item in the middle */}
+        <ul className="navbar__menu">
+          <li className="navbar__item">
+            <a className='navbar__link' href='#login'>Login</a>
+          </li>
+          <li className="navbar__item">
+            <a className='navbar__link' href='#newBooks'>New Books</a>
+          </li>
+          <li className="navbar__item">
+            <a className='navbar__link' href='#genres'>Browse Categories</a>
+          </li>
+        </ul>
+
+        {/* favorite cart on the right */}
+        <button className="navbar__icon">
+          <BsFillBookmarkHeartFill />
+          <span className="navbar__favorite">
+            {/* {favoriteCountries.length} */}
+          </span>
+        </button>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Header
