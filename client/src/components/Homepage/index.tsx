07aa52cd23355db5189c4cc0557c2b827ec5ee2e
binhnhu1409 <binhnhu1409@gmail.com>
2022-06-09 02:07:57 +0300

    frontend: update homepage

--- client/src/components/Homepage/index.tsx
+++ client/src/components/Homepage/index.tsx
@@ -6,11 +6,12 @@ const Homepage = () => {
 
   return (
     <section id='homepage' className='home'>
-      <article className='home__article'>
+      <article className='home__item'>
         <h1 className='home__phrase'>What book <br/> are you looking for?</h1>
       </article>
-      <aside className='home_hero'>
-        <img src="..\..\assets\img\hero_image.jpg" alt="a stack of books with a flower vase on top of it" />
+      {/* import search component here */}
+      <aside className='home__item'>
+        <img className='home__hero' src={require("../../assets/img/hero_image.jpg")} alt="a stack of books with a flower vase on top of it" />
       </aside>
     </section>
   )
